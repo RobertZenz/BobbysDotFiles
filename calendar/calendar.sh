a5429a975b3a1a7558bea2b1b67b528c5a69c95c
Robert Zenz <Bobby@bonsaimind.org>
2012-08-08 15:28:31 +0200

    Added requirements.

--- calendar/calendar.sh
+++ calendar/calendar.sh
@@ -5,9 +5,13 @@
 # it under copyright or a license would be a violation
 # against humanity.
 
+# Requirements;
+# cal, date, sed
 
 # Example usage:
+#
 # ${execp <script> <color>}
+#
 # ${execp ./calendar.sh ffffff}
 
 
