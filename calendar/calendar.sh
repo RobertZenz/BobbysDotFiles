5bd77a9c2991380dcdaa0006f04a1f76b7dbf445
Robert Zenz <Bobby@bonsaimind.org>
2012-08-08 15:26:16 +0200

    Added.

---
+++ calendar/calendar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Public Domain
+# This script is so dead simple that every try to put
+# it under copyright or a license would be a violation
+# against humanity.
+
+
+# Example usage:
+# ${execp <script> <color>}
+# ${execp ./calendar.sh ffffff}
+
+
+cal -h | sed "s/ `date +%_d` / \$\{color #$1\}`date +%_d`\$\{color\} /g"
