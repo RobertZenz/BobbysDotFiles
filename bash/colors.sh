8f61df9876dff38c33cc1edc199047d67de5a30e
Robert Zenz <Bobby@bonsaimind.org>
2013-04-29 20:38:30 +0200

    Fixed.

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
 # Text Color
-TC_BLACK='\e[0;30m'
-TC_RED='\e[0;31m'
-TC_GREEN='\e[0;32m'
-TC_YELLOW='\e[0;33m'
-TC_BLUE='\e[0;34m'
-TC_PURPLE='\e[0;35m'
-TC_CYAN='\e[0;36m'
-TC_WHITE='\e[0;37m'
+TC_BLACK='\e[30m'
+TC_RED='\e[31m'
+TC_GREEN='\e[32m'
+TC_YELLOW='\e[33m'
+TC_BLUE='\e[34m'
+TC_PURPLE='\e[35m'
+TC_CYAN='\e[36m'
+TC_WHITE='\e[37m'
 
-# Text Color Bold
-TCB_BLACK='\e[0;30m'
-TCB_RED='\e[0;31m'
-TCB_GREEN='\e[0;32m'
-TCB_YELLOW='\e[0;33m'
-TCB_BLUE='\e[0;34m'
-TCB_PURPLE='\e[0;35m'
-TCB_CYAN='\e[0;36m'
-TCB_WHITE='\e[0;37m'
+# Background Color
+BC_BLACK='\e[40m'
+BC_RED='\e[41m'
+BC_GREEN='\e[42m'
+BC_YELLOW='\e[43m'
+BC_BLUE='\e[44m'
+BC_PURPLE='\e[45m'
+BC_CYAN='\e[46m'
+BC_WHITE='\e[47m'
 
-# Text Color Underline
-TCU_BLACK='\e[0;30m'
-TCU_RED='\e[0;31m'
-TCU_GREEN='\e[0;32m'
-TCU_YELLOW='\e[0;33m'
-TCU_BLUE='\e[0;34m'
-TCU_PURPLE='\e[0;35m'
-TCU_CYAN='\e[0;36m'
-TCU_WHITE='\e[0;37m'
+# Text Bold
+T_B='\e[1m'
+
+# Text Underline
+T_U='\e[4m'
 
 # Text Color Reset
-TC_RESET='\e[0m'
+T_RESET='\e[0m'
+
